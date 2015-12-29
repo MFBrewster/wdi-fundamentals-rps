29276a497e314186f9813a5df48eaa6dd4ea4fea
Michael Brewster <michaelbrewster@Michael-Brewsters-MacBook-Pro.local>
2015-12-28 22:49:36 -0500

    Accidentally forgot to save rockpaperscissors.js before last commit

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -74,20 +74,20 @@ function playToFive() {
     console.log("Let's play Rock, Paper, Scissors");
     var playerWins = 0;
     var computerWins = 0;
-    var pMove;
-    var cMove;
+    var playerMove;
+    var playerMove;
     var roundWinner;
     // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
     while (playerWins < 5 && computerWins < 5) {
-        pMove = '';
-        cMove = '';
+        playerMove = '';
+        computerMove = '';
         roundWinner = '';
 
-        pMove = getPlayerMove();
-        cMove = getComputerMove();
-        console.log('Player chose ' + pMove + ' while Computer chose ' + cMove);
+        playerMove = getPlayerMove(playerMove);
+        computerMove = getComputerMove(computerMove);
+        console.log('Player chose ' + playerMove + ' while Computer chose ' + computerMove);
         
-        roundWinner = getWinner(pMove, cMove);
+        roundWinner = getWinner(playerMove, computerMove);
         console.log('This round\'s winner is: ' + roundWinner);
         
         if (roundWinner === 'player') { playerWins++; }
